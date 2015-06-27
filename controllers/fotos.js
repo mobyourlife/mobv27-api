7c3d64e8314a6830b8ddd4dd08507f52d4538677
Filipe Oliveira <contato@fmoliveira.com.br>
2015-06-27 11:06:05 -0300

    Paginação de fotos com proteção contra repetição de fotos devido a proximidade do timestamp.

--- controllers/fotos.js
+++ controllers/fotos.js
@@ -2,7 +2,15 @@ var Photo = require('../models/photo');
 
 module.exports = {
 	getFotos: function (req, res) {
-		Photo.find({ ref: req.params.fansite }).sort({ time: -1 }).limit(5).exec(function (err, data) {
+		var args = {};
+		args.ref = req.params.fansite;
+
+		if (req.query.direction && req.query.ne && req.query.time) {
+			args._id = { $ne: req.query.ne };
+			args.time = { $lte: req.query.time };
+		}
+
+		Photo.find(args).sort({ time: -1 }).limit(5).exec(function (err, data) {
 			if (err) {
 				throw err;
 			}
