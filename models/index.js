5f0fcc01416c161358b659f1344c7f3f1b12f871
Filipe Oliveira <contato@fmoliveira.com.br>
2016-03-19 08:42:07 -0300

    Created models index.

---
+++ models/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  album: require('./album'),
+  domain: require('./domain'),
+  fanpage: require('./fanpage'),
+  feed: require('./feed'),
+  mailing: require('./mailing'),
+  outmail: require('./outmail'),
+  photo: require('./photo'),
+  textpage: require('./textpage'),
+  ticket: require('./ticket'),
+  update: require('./update'),
+  user: require('./user'),
+  video: require('./video')
+}
